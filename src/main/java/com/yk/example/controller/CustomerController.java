94e7b4a7980d865a1e266bf97326ea8a18444d0c
余奎 <292528867@qq.com>
2015-12-15 17:34:10 +0800

    elasticsearch 修改中

--- src/main/java/com/yk/example/controller/CustomerController.java
+++ src/main/java/com/yk/example/controller/CustomerController.java
@@ -1,6 +1,12 @@
 package com.yk.example.controller;
 
+import com.yk.example.elasticsearch.Customer;
+import com.yk.example.elasticsearch.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,16 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("customer")
 public class CustomerController {
 
-//    @Autowired
-//    private CustomerRepository customerRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
 
-//    @RequestMapping(value = "saveCustomer",method = RequestMethod.POST)
-//    public Object saveCustomer(@RequestBody Customer customer) {
-//        return customerRepository.save(customer);
-//    }
-//
-//    @RequestMapping(value = "fetchAllCustomers",method = RequestMethod.GET)
-//    public Object fetchAllCustomers() {
-//        return customerRepository.findAll();
-//    }
+    @RequestMapping(value = "saveCustomer",method = RequestMethod.POST)
+    public Object saveCustomer(@RequestBody Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    @RequestMapping(value = "fetchAllCustomers",method = RequestMethod.GET)
+    public Object fetchAllCustomers() {
+        return customerRepository.findAll();
+    }
 }
--- src/main/java/com/yk/example/elasticsearch/CustomerRepository.java
+++ src/main/java/com/yk/example/elasticsearch/CustomerRepository.java
@@ -1,12 +1,16 @@
 package com.yk.example.elasticsearch;
 
 
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
 /**
- * Created by yk on 15/12/9.
- */
-//public interface CustomerRepository extends ElasticsearchRepository<Customer,Long> {
-//
-//    public Customer findByName(String name);
-//
-//    public List<Customer> findByLastName(String lastName);
-//}
+* Created by yk on 15/12/9.
+*/
+public interface CustomerRepository extends ElasticsearchRepository<Customer,Long> {
+
+    public Customer findByName(String name);
+
+    public List<Customer> findByLastName(String lastName);
+}
